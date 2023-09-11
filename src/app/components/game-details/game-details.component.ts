063af60c8e1bad5a186efde8dba56a72109de3f7
Niko <germano.niko01@gmail.com>
2023-09-11 17:48:10 -0400

    last commit

---
+++ src/app/components/game-details/game-details.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
+
+@Component({
+  selector: 'app-game-details',
+  templateUrl: './game-details.component.html',
+  styleUrls: ['./game-details.component.scss'],
+})
+export class GameDetailsComponent implements OnInit, OnDestroy {
+  gameRating = 0;
+  game!: Game;
+  gameId!: string;
+  routeSub!: Subscription;
+  gameSub!: Subscription;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private httpService: HttpService
+  ) {}
+
+  ngOnInit(): void {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
+      this.gameId = params['id'];
+      this.getGameDetails(this.gameId);
+    });
+  }
+
+  getGameDetails(id: string): void {
+    this.gameSub = this.httpService
+      .getGameDetails(id)
+      .subscribe((gameResponse: Game) => {
+        this.game = gameResponse;
+      });
+  }
+
+  getColor(value: number): string {
+    if (value > 75) {
+      return '#5ee432';
+    } else if (value > 50) {
+      return '#fffa50';
+    } else if (value > 30) {
+      return '#f7aa38';
+    } else {
+      return '#ef4655';
+    }
+  }
+
+  ngOnDestroy(): void {
+    // Unsubscribe from the subscriptions to prevent memory leaks
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+  }
+}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { APIResponse, Game } from 'src/app/models';
 import { HttpService } from 'src/app/services/http.service';
 
@@ -8,30 +9,51 @@ import { HttpService } from 'src/app/services/http.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public sort: string = '';
   public games: Array<Game> = [];
+  private routeSub: Subscription | undefined;
+  private gameSub: Subscription | undefined;
 
   constructor(
     private httpService: HttpService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {}
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params: Params) => {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
       if (params['game-search']) {
-        this.searchGames('metacritic', params['game-search']);
+        this.searchGames('-added', params['game-search']);
       } else {
-        this.searchGames('metacritic');
+        this.searchGames('-added');
       }
     });
   }
 
+  //search all games
   searchGames(sort: string, search?: string): void {
-    this.httpService
+    this.gameSub = this.httpService
       .getGamesList(sort, search)
       .subscribe((gameList: APIResponse<Game>) => {
         this.games = gameList.results;
         console.log(gameList);
       });
+  }
+
+  //open game details page
+  openGameDetails(gameId: string): void {
+    //send id in the parameters of a new route
+    this.router.navigate(['details', gameId]);
+    
+  }
+
+  //clean up function that prevents memory leaks
+  ngOnDestroy(): void {
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
   }
 }
